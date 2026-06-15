009b35123c08c48c6ff5c14ab0cb26ac9c4e628a
dreiii-ue <eduarte.jamelandrei@ue.edu.ph>
2026-06-15 14:32:21 +0800

    update

--- includes/config.php
+++ includes/config.php
@@ -1,12 +1,6 @@
 <?php
 
-$conn = mysqli_connect(
-    "db.fr-pari1.bengt.wasmernet.com",
-    "user_d4399a30",
-    "pw_11751dfd",
-    "db_10580911",
-    10272
-);
+$conn = mysqli_connect("db.fr-pari1.bengt.wasmernet.com", "user_d4399a30", "pw_11751dfd", "db_10580911", 10272);
 
 if(!$conn){
     die("Database connection failed");
